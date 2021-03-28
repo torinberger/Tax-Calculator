d80310548552ba8429b91b2080c1037af182db2a
David Steedman <dave@voxace.com>
2021-03-28 21:07:27 +1100

    Initial commit

---
+++ Assets/Scripts/TaxCalculator.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using SpeechLib;
+
+public class TaxCalculator : MonoBehaviour
+{
+    // Constant rate for the Medicare Levy
+    const double MEDICARE_LEVY = 0.02;
+
+    // Variables
+    bool textToSpeechEnabled = true;
+
+    private void Start()
+    {
+        Speak("Welcome to the A.T.O. Tax Calculator");
+    }
+
+    // Run this function on the click event of your 'Calculate' button
+    public void Calculate()
+    {
+        // Initialisation of variables
+        double medicareLevyPaid = 0;
+        double incomeTaxPaid = 0;
+
+        // Input
+        double grossSalaryInput = GetGrossSalary();
+        string salaryPayPeriod = GetSalaryPayPeriod();
+
+        // Calculations
+        double grossYearlySalary = CalculateGrossYearlySalary(grossSalaryInput, salaryPayPeriod);
+        double netIncome = CalculateNetIncome(grossYearlySalary, ref medicareLevyPaid, ref incomeTaxPaid);
+
+        // Output
+        OutputResults(medicareLevyPaid, incomeTaxPaid, netIncome);
+    }
+
+    private double GetGrossSalary()
+    {
+        // Get from user. E.g. input box
+        // Validate the input (ensure it is a positive, valid number)
+        double grossYearlySalary = 1000;
+        return grossYearlySalary;
+    }
+
+    private string GetSalaryPayPeriod()
+    {
+        // Get from user. E.g. combobox or radio buttons
+        string salaryPayPeriod = "weekly";
+        return salaryPayPeriod;
+    }
+
+    private double CalculateGrossYearlySalary(double grossSalaryInput, string salaryPayPeriod)
+    {
+        // This is a stub, replace with the real calculation and return the result
+        double grossYearlySalary = 50000;
+        return grossYearlySalary;
+    }
+
+    private double CalculateNetIncome(double grossYearlySalary, ref double medicareLevyPaid, ref double incomeTaxPaid)
+    {
+        // This is a stub, replace with the real calculation and return the result
+        medicareLevyPaid = CalculateMedicareLevy(grossYearlySalary);
+        incomeTaxPaid = CalculateIncomeTax(grossYearlySalary);
+        double netIncome = 33000;        
+        return netIncome;
+    }
+
+    private double CalculateMedicareLevy(double grossYearlySalary)
+    {
+        // This is a stub, replace with the real calculation and return the result
+        double medicareLevyPaid = 2000;        
+        return medicareLevyPaid;
+    }
+
+    private double CalculateIncomeTax(double grossYearlySalary)
+    {
+        // This is a stub, replace with the real calculation and return the result
+        double incomeTaxPaid = 15000;
+        return incomeTaxPaid;
+    }
+
+    private void OutputResults(double medicareLevyPaid, double incomeTaxPaid, double netIncome)
+    {
+        // Output the following to the GUI
+        // "Medicare levy paid: $" + medicareLevyPaid.ToString("F2");
+        // "Income tax paid: $" + incomeTaxPaid.ToString("F2");
+        // "Net income: $" + netIncome.ToString("F2");
+    }
+
+    // Text to Speech
+    private void Speak(string textToSpeak)
+    {
+        if(textToSpeechEnabled)
+        {
+            SpVoice voice = new SpVoice();
+            voice.Speak(textToSpeak);
+        }
+    }
+}
